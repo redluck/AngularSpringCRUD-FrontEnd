fc9a2853ed123d4fff02ae2d8fcfa2fa3a1b8364
redluck <redluck@tiscali.it>
2018-04-30 14:52:28 +0200

    Primo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-
+import { ArticleComponent } from './article.component';
+import { ArticleService } from './article.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ArticleComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpModule,
+		ReactiveFormsModule
+	],
+	providers: [
+		ArticleService
+	],
+	bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }
